eb84e6549ed312e46e20c107c2514a782f242786
Nicholas Clawson <nickclaw@gmail.com>
2013-03-05 17:00:04 -0800

    Added template support
    You can now define all the objects inside a post via a template, using
    '{image}' as a placeholder that will be filled with the current
    dataobjects .image url.

--- infinitescrolling.js
+++ infinitescrolling.js
@@ -1,11 +1,29 @@
 var postLoader;
 
-function PostLoader(startingIndex, posts, container) {
+function PostLoader(startingIndex, posts, container, postClass, template) {
 	this.index = startingIndex;
 	this.data = posts;
 	this.parent = container;
-	isLoading = false;
-	isAtEnd = this.index < posts.length;
+	this.isLoading = false;
+	this.isAtEnd = this.index < posts.length;
+	this.postClass = postClass;
+	this.template = template;
+
+	this.templateLoad = function(n) {
+		for (var i = this.index; i - this.index < n && i < this.data.length; i++){
+			var post = document.createElement('div');
+				post.classList.add(this.postClass);
+				var a = this.template;
+				while(a.indexOf('{') > 0){
+					a = a.substring(0, a.indexOf('{')) + eval('this.data[' + i + '].' + a.substring(a.indexOf('{')+1, a.indexOf('}'))) + a.substring(a.indexOf('}') +1)
+				}
+				post.innerHTML = a;
+			this.parent.appendChild(post);
+		}
+		this.index += n;
+		this.isAtEnd = this.index < posts.length;
+		this.isLoading = false;
+	}
 
 	this.load = function(n) {
 		this.isLoading = true;
@@ -37,8 +55,9 @@ function PostLoader(startingIndex, posts, container) {
 window.onload = function() {
 	var postsContainer = document.getElementById('posts');
 	var posts = getPosts();
-	postLoader = new PostLoader(0, posts, postsContainer);
-	postLoader.load(4);
+	var template = '<div class="picture" style="background-image: url({image});"></div><div class="text">{subtext}<div class="more">Read More...</div></div>'
+	postLoader = new PostLoader(0, posts, postsContainer, 'post', template);
+	postLoader.templateLoad(4);
 
 	document.addEventListener('scroll', onScroll);
 }
@@ -53,6 +72,6 @@ function getPosts(){
 function onScroll(event){
 	var scrolled = window.scrollY/(document.height-window.innerHeight);
 	if (scrolled > .98 && !postLoader.isLoading){
-		postLoader.load(1);
+		postLoader.templateLoad(1);
 	}
 }
