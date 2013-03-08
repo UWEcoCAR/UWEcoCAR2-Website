b014a69b5ef3ddd4066cb3892a7c226b80387114
Nicholas Clawson <nickclaw@gmail.com>
2013-03-08 11:55:58 -0800

    Infinite scrolling code cleanup
    Got rid of unused post-open.css, made the PostLoader slightly more
    generic, cleaned up posts.css a little bit.

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -1,5 +1,5 @@
 require(["jquery", "infinitescrolling"], function() {
 	var postTemplate = '<div class="picture" style="background-image: url({image});"></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a href="#test" class="more" index="{title}">Read More...</a></div>';
-	postLoader = new PostLoader('homePosts.json', $('#posts'), postTemplate);
+	postLoader = new PostLoader('homePosts.json', $('#posts'), postTemplate, 'post');
 	postLoader.load(2);
 });
--- www/scripts/infinitescrolling.js
+++ www/scripts/infinitescrolling.js
@@ -1,17 +1,11 @@
 var postLoader;
 
-function PostLoader(postFile, container, template) {
+function PostLoader(postFile, container, template, mainClass) {
 	this.index = 0;
 	this.data = getPosts(postFile);
 	this.parent = container;
 	this.template = template;
-
-//	this.mask = document.createElement('div');
-//		this.mask.id = 'mask';
-//		$(this.mask).hide().click(function(){postLoader.setPost(false)})
-//	this.parent.appendChild(this.mask);
-
-//	this.post = new Post(this.parent);
+	this.class = mainClass;
 
 	this.isLoading = false;
 	this.isAtEnd = this.index < posts.length;
@@ -19,10 +13,11 @@ function PostLoader(postFile, container, template) {
 
 
 	this.load = function(n) {
+		this.isLoading = true;
 		var i = this.index;
 		while (i - this.index < n && i < this.data.length){
 			var post = document.createElement('div');
-				post.classList.add('post');
+				post.classList.add(this.class);
 				var a = this.template;
 				while(a.indexOf('{') > 0){
 					a = a.substring(0, a.indexOf('{')) + eval('this.data[' + i + '].' + a.substring(a.indexOf('{')+1, a.indexOf('}'))) + a.substring(a.indexOf('}') +1)
@@ -45,41 +40,10 @@ function PostLoader(postFile, container, template) {
 		var me = e.target.scope;
 		var index = e.target.index;
 		$("<p>" + me.data[index].text + "</p>").appendTo($(this).parent());
-//		$('<a href="#test" class="more">Read Less...</a>').click(function() {
-//			
-//		}).appendTo($(this).parent());
 		$(this).remove();
-		//me.setPost(me.data[index]);
 	}
-
-//	this.setPost = function(data){
-//		if (data){
-//			$('#mask').show();
-//			this.post.load(data);
-//			$(this.post.object).show();
-//		} else {
-//			$('#mask').hide();
-//			$(this.post.object).hide();
-//		}
-//	}
 }
 
-//function Post(container){
-//	this.template = '<div id="openedPost"><div id="openedPostImage" style="background-image: url({image})"></div><div id="openedPostText">{text}</div></div>'
-//
-//	this.object = document.createElement('div');
-//		this.object.id = 'openedPostContainer';
-//		$(this.object).hide();
-//		container.appendChild(this.object);
-//
-//	this.load = function(data) {
-//		var a = this.template;
-//		while(a.indexOf('{') > 0){
-//			a = a.substring(0, a.indexOf('{')) + eval('data.' + a.substring(a.indexOf('{')+1, a.indexOf('}'))) + a.substring(a.indexOf('}') +1)
-//		}
-//		this.object.innerHTML = a;
-//	}
-//}
 
 function getPosts(postFile){
 	var	request = new XMLHttpRequest();
@@ -90,17 +54,12 @@ function getPosts(postFile){
 
 $(document).scroll(function(event){
 	var scrolled = window.scrollY/(document.height-window.innerHeight);
-	if (scrolled > .98 && !postLoader.isLoading){
+	if (scrolled > .96 && !postLoader.isLoading){
 		postLoader.load(1);
 	}
 });
 
 $("<link/>").attr({
 	href: "css/posts.css",
-	rel: "stylesheet"
-}).appendTo("head");
-
-$("<link/>").attr({
-	href: "css/post-open.css",
 	rel: "stylesheet"
 }).appendTo("head");
