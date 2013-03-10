9cce119a6dc23f8e58a0bc014023998e64c400aa
Mitchell Loeppky <mitchell@loeppky.com>
2013-03-09 19:01:48 -0800

    Added a sub header to the team page and added more bios
    Also made some general styling changes

--- www/scripts/team.js
+++ www/scripts/team.js
@@ -1,5 +1,5 @@
 require(["jquery", "infinitescrolling"], function() {
-	var postTemplate = '<img class="portrait" src="{image}" /></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a href="#test" class="more" index="{title}">Read More...</a></div>';
+	var postTemplate = '<img class="portrait" src="{image}" /></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a role="button" class="more" index="{title}">Read More...</a></div>';
 	postLoader = new PostLoader('teamPosts.json', $('#posts'), postTemplate, 'post');
 	postLoader.load(2);
 
