d1883e505117c755122a19ed05025287fd74eb01
Tore Hanssen <tk.hanssen@gmail.com>
2013-03-10 23:21:52 -0700

    Working version with proper file format

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -1,4 +1,4 @@
-require(["jquery", "infinitescrolling"], function() {
+require(["jquery", "infinitescrolling", "interactivecar"], function() {
 	var postTemplate = '<div class="picture" style="background-image: url({image});"></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a href="#test" class="more" index="{title}">Read More...</a></div>';
 	postLoader = new PostLoader('homePosts.json', $('#posts'), postTemplate, 'post');
 	postLoader.load(2);
