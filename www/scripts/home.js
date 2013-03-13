bf0d9a3ab5b1eda3523b13cbcd6f11d373d0d5ce
Nicholas Clawson <nickclaw@gmail.com>
2013-03-13 16:00:24 -0700

    Autoload
    Infinite scrolling will now automatically autoload until the '#posts'
    element is just off the screen.

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -6,5 +6,5 @@ require(["jquery", "infinitescrolling", "interactivecar"], function() {
     
     var postTemplate = '<img class="picture framed" src="{image}" /><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subtitle}</h2><h3 class="title">{date}</h3><p>{subtext}</p><a href="#test" class="more" index="{title}">Read More...</a></div>';
     postLoader = new PostLoader('homePosts.json', $('#posts'), postTemplate, 'post');
-    postLoader.load(2);
+    postLoader.autoload();
 });
