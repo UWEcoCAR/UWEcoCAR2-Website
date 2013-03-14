a00fc5f9e8dcc989fcc6e41de3769000f2e3900f
Tore Hanssen <tk.hanssen@gmail.com>
2013-03-13 19:07:10 -0700

    Fade functionaltiy for car

--- www/scripts/interactivecar.js
+++ www/scripts/interactivecar.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
 
 
-	$('#batteryMap').hover(function() {
-		$('#moreInfo').attr('class', 'interactivecarBattery');
-	});
+	$('#batteryMap').hover(
+        function() {
+            $('#moreInfo').hide();
+            $('#moreInfo').attr('class', 'interactivecarBattery');
+    		$('#moreInfo').fadeIn();
+        },
+        function () {
+            $('#moreInfo').fadeOut(500, function() {
+                $('#moreInfo').attr('class', '');
+                $('#moreInfo').show();
+            });
+        }
+    );
 
-	$('#engineMap').hover(function() {
-		$('#moreInfo').attr('class', 'interactivecarEngine');
-	});
-}); 
+	$('#engineMap').hover(
+        function() {
+            $('#moreInfo').hide();
+            $('#moreInfo').attr('class', 'interactivecarEngine');
+            $('#moreInfo').fadeIn();
+        },
+        function () {
+            $('#moreInfo').fadeOut(500, function() {
+                $('#moreInfo').attr('class', '');
+                $('#moreInfo').show();
+            });
+        }
+    );
+});
