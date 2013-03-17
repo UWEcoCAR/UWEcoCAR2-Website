1cdf78de24b0638d5279ad7526b519d93da1ecd0
Tore Hanssen <tk.hanssen@gmail.com>
2013-03-17 00:45:27 -0700

    Each part now accounted for an text transitions

--- www/scripts/interactivecar.js
+++ www/scripts/interactivecar.js
@@ -1,11 +1,96 @@
 $(document).ready(function(){
+    $('#infoContainer').children().hide();
+    $('#partInfoText').children().hide();
 
 
-    $('#batteryMap').hover(function() {
-        $('#moreInfo').attr('class', 'interactivecarBattery');
-    });
+    $('#interactivecar').hover(
+        function() {
+            $('#closedCar').fadeOut();
+        },
+        function () {
+            $('#closedCar').fadeIn();
+        }
+    );
 
-    $('#engineMap').hover(function() {
-        $('#moreInfo').attr('class', 'interactivecarEngine');
-    });
-}); 
+    $('#batteryMap').hover(
+        function() {
+            partHover("#battery", "Battery")
+        }, function() {
+            partHoverEnd("#battery")
+        }
+    );
+
+    $('#engineMap').hover(
+        function() {
+            partHover("#engine", "Engine")
+        }, function() {
+            partHoverEnd("#engine")
+        }
+    );
+
+    $('#gearboxMap').hover(
+        function() {
+            partHover("#gearbox", "Gearbox")
+        }, function() {
+            partHoverEnd("#gearbox")
+        }
+    );
+
+    $('#inverterMap').hover(
+        function() {
+            partHover("#inverter", "Inverter")
+        }, function() {
+            partHoverEnd("#inverter")
+        }
+    );
+
+    $('#motorMap').hover(
+        function() {
+            partHover("#motor", "Motor")
+        }, function() {
+            partHoverEnd("#motor")
+        }
+    );
+
+    $('#tankMap').hover(
+        function() {
+            partHover("#tank", "Tank")
+        }, function() {
+            partHoverEnd("#tank")
+        }
+    );
+
+    $('#transMap').hover(
+        function() {
+            partHover("#trans", "Transmission")
+        }, function() {
+            partHoverEnd("#trans")
+        }
+    );
+
+    $('#ureaMap').hover(
+        function() {
+            partHover("#urea", "Urea")
+        }, function() {
+            partHoverEnd("#urea")
+        }
+    );
+});
+
+
+/*  Called when one of the defined map areas is hovered on
+    Displays the highlighted image and the text using the given
+    base id (eg #urea) and text */
+function partHover(backgroundStyle, text) {
+    $(backgroundStyle + "Info").fadeIn();
+    $(backgroundStyle + "InfoText").html(text);
+    $(backgroundStyle + "InfoText").fadeIn();
+}
+
+
+/*  Called when one of the defined map areas is hovered off of
+    hides all information pretaining to the previously hovered area */
+function partHoverEnd(backgroundStyle) {
+    $(backgroundStyle + "InfoText").fadeOut();
+    $(backgroundStyle + "Info").fadeOut();
+}
