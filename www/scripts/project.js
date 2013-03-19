eab2e12d6ae865e63852392761df8d4a6e981ec6
Mitchell Loeppky <mitchell@loeppky.com>
2013-03-19 15:44:40 -0700

    Added more sections to project page

--- www/scripts/project.js
+++ www/scripts/project.js
@@ -1,5 +1,5 @@
 require(["jquery", "infinitescrolling"], function() {
-    var postTemplate = '<img class="{imageClass}" src="{image}" /></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a role="button" class="more" index="{title}">Read More...</a></div>';
+    var postTemplate = '<img class="{imageClass}" src="{image}" />{other}</div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a role="button" class="more" index="{title}">Read More...</a></div>';
     postLoader = new PostLoader('projectPosts.json', $('#posts'), postTemplate, 'post');
     postLoader.autoload();
 
