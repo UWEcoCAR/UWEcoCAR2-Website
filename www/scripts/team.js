18a0dbda9c42732d5290249465f64623348a5c28
Mitchell Loeppky <mloeppky@Mitchells-MacBook-Pro.local>
2013-04-16 22:43:29 -0700

    Added Taj and Mitchell's pictures
    Also added Mitchell's Bio and fixed a typo

--- www/scripts/team.js
+++ www/scripts/team.js
@@ -1,5 +1,5 @@
 require(["jquery", "infinitescrolling", "common"], function() {
-    var imgNum = Math.floor(Math.random() * 3) + 1;
+    var imgNum = Math.floor((new Date().getTime() / 3000) % 3) + 1; // The image that will be loaded changes every three seconds
     var postTemplate = '<img class="portrait" src="{image' + imgNum + '}" /></div><div class="text"><h1 class="title">{title}</h1><h2 class="title">{subTitle}</h2><p>{subtext}</p><a role="button" class="more" index="{title}">Read More...</a></div>';
     postLoader = new PostLoader('teamPosts.json', $('#posts'), postTemplate, 'post');
     postLoader.autoload();
